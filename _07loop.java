89fd11144f37cc4fc9314b52d80b931f03b918e9
Pratyusha1618O <pratyushabhandary2003@gmail.com>
2024-09-29 23:38:08 +0530

    Loops

--- _07loop.java
+++ _07loop.java
@@ -11,5 +11,19 @@ public class _07loop {
             System.out.println(i);
             i++;
         }
+
+        //sum of series
+        int sum = 0;
+        for(i=1; i<=n; i++){
+            sum = sum + i;
+        }
+        System.out.println("Sum of series: " + sum);
+
+        //sum of even
+        int even = 0;
+        for(i=2; i<=n; i+=2){
+            even = even + i;
+        }
+        System.out.println("even sum: " + even);
     }
 }
