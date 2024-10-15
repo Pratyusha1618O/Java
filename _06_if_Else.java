e3979da854a73c7091903e14191c3d22f7d3fc92
Pratyusha1618O <pratyushabhandary2003@gmail.com>
2024-10-15 20:50:17 +0530

    Conditional statement

--- _06_if_Else.java
+++ _06_if_Else.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class _06odd_even {
+public class _06_if_Else {
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
         System.out.print("Enter the number: ");
