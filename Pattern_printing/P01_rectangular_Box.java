2290390e040bd926e398caa9b7fe42c064f02fa4
Pratyusha1618O <pratyushabhandary2003@gmail.com>
2024-10-15 08:26:48 +0530

    Pattern Printing

--- Pattern_printing/P01_rectangular_Box.java
+++ Pattern_printing/P01_rectangular_Box.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class P01_starBox {
+public class P01_rectangular_Box {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the term: ");
@@ -15,5 +15,6 @@ public class P01_starBox {
             System.out.println();
         }
 
+        sc.close();
     }
 }
