1340e1980489445a4eee25c5108370fd2492828a
Pratyusha1618O <pratyushabhandary2003@gmail.com>
2024-10-15 09:26:11 +0530

    number system

---
+++ Number_system/binary_to_decimal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class binary_to_decimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Binary: ");
+        int binary = sc.nextInt();
+
+        int ans = 0;//converted decimal number
+        int pw = 1; //2^0 = 1,, power of 2
+
+        while(binary > 0)
+        {
+            int unit_digit = binary % 10;
+            ans += (unit_digit * pw );
+            binary = binary/10;
+            pw *= 2;
+        }
+
+        System.out.println("Decimal : "+ ans);
+    }
+}
