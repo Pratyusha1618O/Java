6ec4a91a22d526b8ae31200639a53664972e8ff0
Pratyusha1618O <pratyushabhandary2003@gmail.com>
2024-10-18 23:57:30 +0530

    Armstrong Number

---
+++ Basic_javaPrograms/java07_armstrong.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class java07_armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+
+        int copy = n;
+        int digit_count = 0;
+        while(n>0)
+        {
+            n = n / 10;
+            digit_count++;
+        }
+
+        // System.out.println("digits: "+ digit_count);
+        n = copy;
+
+        int sum = 0;
+        while(n>0)
+        {
+            sum += Math.pow(n%10, digit_count);
+            n = n / 10;
+        }
+
+        if(copy == sum ){
+            System.out.println("The number is Armstrong Number");
+        }
+        else{
+            System.out.println("The number is Not Armstrong Number");
+        }
+
+        sc.close();
+    }
+}
